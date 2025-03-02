88a0145100146358ae9bce84634251e94cad64cb
Ackooo <aleksandarspetrovic1@gmail.com>
2025-03-02 20:38:14 +0100

    git auth fix extensions

--- API/Program.cs
+++ API/Program.cs
@@ -15,9 +15,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
-//treba dodati kontrolere
-
-
 //service container
 //order does not matter
 
