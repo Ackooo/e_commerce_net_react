52be217ea65ad60399642ed05990f9b59d39554a
Aleksa Ojdanic <aojdanic97@gmail.com>
2025-04-15 22:39:20 +0200

    change base url

--- client/web_react/src/app/api/agent.ts
+++ client/web_react/src/app/api/agent.ts
@@ -4,133 +4,165 @@ import { router } from "../router/Routes";
 import { PaginatedResponse } from "../models/pagination";
 import { store } from "../store/configureStore";
 
-const simulateDelay = () => new Promise(resolve => setTimeout(resolve, 500));
+const simulateDelay = () => new Promise((resolve) => setTimeout(resolve, 500));
 //TODO: config api url
-axios.defaults.baseURL = 'https://localhost:5000/api/';
+axios.defaults.baseURL =
+  "https://e-commerce-net-react-api-fec2enesbxa6geb3.germanywestcentral-01.azurewebsites.net/api/";
 axios.defaults.withCredentials = true;
 const responseBody = (response: AxiosResponse) => response.data;
 
-axios.interceptors.request.use(config=>{
-    const token = store.getState().account.user?.token;
-    if(token) config.headers.Authorization = `Bearer ${token}`;
-    return config;
-})
+axios.interceptors.request.use((config) => {
+  const token = store.getState().account.user?.token;
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
 
-axios.interceptors.response.use(async response => {
+axios.interceptors.response.use(
+  async (response) => {
     //TODO: testing purpose
     await simulateDelay();
 
     //lower case because of axios properties
-    const pagination = response.headers['pagination'];
-    if(pagination){
-        response.data = new PaginatedResponse(response.data, JSON.parse(pagination));       
+    const pagination = response.headers["pagination"];
+    if (pagination) {
+      response.data = new PaginatedResponse(
+        response.data,
+        JSON.parse(pagination),
+      );
     }
-    
-    return response
-}, (error: AxiosError) => {
+
+    return response;
+  },
+  (error: AxiosError) => {
     const { data, status } = error.response as AxiosResponse;
     switch (status) {
-        case 400:
-            if (data.errors) {
-                const modelStateErrors: string[] = [];
-                for (const key in data.errors) {
-                    if (data.errors[key]) {
-                        modelStateErrors.push(data.errors[key])
-                    }
-                }
-                throw modelStateErrors.flat();
+      case 400:
+        if (data.errors) {
+          const modelStateErrors: string[] = [];
+          for (const key in data.errors) {
+            if (data.errors[key]) {
+              modelStateErrors.push(data.errors[key]);
             }
-            toast.error(data.title); break;
-        case 401:
-            toast.error(data.title ); break;
-        case 403:
-            toast.error(data.title ); 
-            toast.error('Not allowed!'); break;
-        case 500:
-            //only way to add something in history of the browser outside the react component
-            router.navigate('/server-error', { state: { error: data } });
-            break;
-        default:
-            break;
+          }
+          throw modelStateErrors.flat();
+        }
+        toast.error(data.title);
+        break;
+      case 401:
+        toast.error(data.title);
+        break;
+      case 403:
+        toast.error(data.title);
+        toast.error("Not allowed!");
+        break;
+      case 500:
+        //only way to add something in history of the browser outside the react component
+        router.navigate("/server-error", { state: { error: data } });
+        break;
+      default:
+        break;
     }
     return Promise.reject(error.response);
-})
+  },
+);
 
 const requests = {
-    get: (url: string, params?: URLSearchParams) => axios.get(url, { params }).then(responseBody),
-    post: (url: string, body: {}, p0?: { withCredentials: boolean; }) => axios.post(url, body).then(responseBody),
-    put: (url: string, body: {}) => axios.put(url, body).then(responseBody),
-    delete: (url: string) => axios.delete(url).then(responseBody),
-    postForm: (url:string, data:FormData) => axios.post(url, data, {
-        headers: {'Content-type':'multipart/form-data'}
-    }).then(responseBody),
-    putForm: (url:string, data:FormData) => axios.put(url, data, {
-        headers: {'Content-type':'multipart/form-data'}
-    }).then(responseBody)
+  get: (url: string, params?: URLSearchParams) =>
+    axios.get(url, { params }).then(responseBody),
+  post: (url: string, body: {}, p0?: { withCredentials: boolean }) =>
+    axios.post(url, body).then(responseBody),
+  put: (url: string, body: {}) => axios.put(url, body).then(responseBody),
+  delete: (url: string) => axios.delete(url).then(responseBody),
+  postForm: (url: string, data: FormData) =>
+    axios
+      .post(url, data, {
+        headers: { "Content-type": "multipart/form-data" },
+      })
+      .then(responseBody),
+  putForm: (url: string, data: FormData) =>
+    axios
+      .put(url, data, {
+        headers: { "Content-type": "multipart/form-data" },
+      })
+      .then(responseBody),
+};
+
+function createFormData(item: any) {
+  let formData = new FormData();
+  for (const key in item) {
+    formData.append(key, item[key]);
+  }
+  return formData;
 }
 
-function createFormData(item:any){
-    let formData = new FormData();
-    for(const key in item){
-        formData.append(key, item[key])
-    }
-    return formData;
-}
-
-const Vendor={
-    createProduct: (product:any) =>requests.postForm('products', createFormData(product)),
-    updateProduct: (product:any) =>requests.putForm('products', createFormData(product)),
-    deleteProduct: (id: number) => requests.delete(`products/${id}`),
-}
+const Vendor = {
+  createProduct: (product: any) =>
+    requests.postForm("products", createFormData(product)),
+  updateProduct: (product: any) =>
+    requests.putForm("products", createFormData(product)),
+  deleteProduct: (id: number) => requests.delete(`products/${id}`),
+};
 
 const Catalog = {
-    list: (params: URLSearchParams) => requests.get('products', params),
-    details: (id: number) => requests.get(`products/${id}`),
-    fetchFilters: () => requests.get('products/filters')
-}
+  list: (params: URLSearchParams) => requests.get("products", params),
+  details: (id: number) => requests.get(`products/${id}`),
+  fetchFilters: () => requests.get("products/filters"),
+};
 
 const TestErrors = {
-    get400Error: () => requests.get('buggy/bad-request'),
-    get401Error: () => requests.get('buggy/unauthorised'),
-    get404Error: () => requests.get('buggy/not-found'),
-    get500Error: () => requests.get('buggy/server-error'),
-    getValidationError: () => requests.get('buggy/validation-error'),
-}
+  get400Error: () => requests.get("buggy/bad-request"),
+  get401Error: () => requests.get("buggy/unauthorised"),
+  get404Error: () => requests.get("buggy/not-found"),
+  get500Error: () => requests.get("buggy/server-error"),
+  getValidationError: () => requests.get("buggy/validation-error"),
+};
 
 const Basket = {
-    get: () => requests.get('basket'),
-    addItem: (productId: number, quantity = 1) => requests.post(`basket/additem?productId=${productId}&quantity=${quantity}`, {}),
-    removeItem: (productId: number, quantity = 1) => requests.delete(`basket/removeItem?productId=${productId}&quantity=${quantity}`)
-}
+  get: () => requests.get("basket"),
+  addItem: (productId: number, quantity = 1) =>
+    requests.post(
+      `basket/additem?productId=${productId}&quantity=${quantity}`,
+      {},
+    ),
+  removeItem: (productId: number, quantity = 1) =>
+    requests.delete(
+      `basket/removeItem?productId=${productId}&quantity=${quantity}`,
+    ),
+};
 
 const Account = {
-    login: (values: any) => requests.post('account/login', values),
-    register: (values: any) => requests.post('account/register', values),
-    currentUser: () => requests.get('account/currentUser'),
-    fetchAddress: () => requests.get('account/savedAddress'),
-    fetchCultures: () => requests.get('account/availableCultures'),
-    setCulture: (value: any) => requests.post(`account/setCulture?culture=${value}`, {}, { withCredentials: true }),
-
-}
+  login: (values: any) => requests.post("account/login", values),
+  register: (values: any) => requests.post("account/register", values),
+  currentUser: () => requests.get("account/currentUser"),
+  fetchAddress: () => requests.get("account/savedAddress"),
+  fetchCultures: () => requests.get("account/availableCultures"),
+  setCulture: (value: any) =>
+    requests.post(
+      `account/setCulture?culture=${value}`,
+      {},
+      { withCredentials: true },
+    ),
+};
 
 const Orders = {
-    list: () => requests.get('orders'),
-    fetch: (id:string) => requests.get(`orders/${id}`),
-    create: (values:any) => requests.post('orders', values)
-}
+  list: () => requests.get("orders"),
+  fetch: (id: string) => requests.get(`orders/${id}`),
+  create: (values: any) => requests.post("orders", values),
+};
 
 const Payments = {
-    createPaymentIntent: () => requests.post('payments', {})
-}
+  createPaymentIntent: () => requests.post("payments", {}),
+};
 
 const agent = {
-    Catalog,
-    TestErrors,
-    Basket,
-    Account,
-    Orders,
-    Payments,
-    Vendor
-}
+  Catalog,
+  TestErrors,
+  Basket,
+  Account,
+  Orders,
+  Payments,
+  Vendor,
+};
 
 export default agent;
+
